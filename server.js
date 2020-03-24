d938b0277a57ccf3bdd539ab0bcc2e154dd886d0
cavner <katieavner@gmail.com>
2020-03-23 20:35:50 -0700

    more files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,14 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  "mongodb://claurie:si11y#b0y@ds219078.mlab.com:19078/heroku_hdsdbrwx",
+  {
+    useMongoClient: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Server listening on: http://localhost:3000 .`);
 });
