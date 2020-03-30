8a820d2ac812554f35ea5c8f083a7053b7d762d7
cavner <katieavner@gmail.com>
2020-03-30 12:51:07 -0700

    added service-workers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
@@ -15,18 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  "mongodb://claurie:si11y#b0y@ds219078.mlab.com:19078/heroku_hdsdbrwx",
-  {
-    useMongoClient: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:3000 .`);
+  console.log(`App running on port ${PORT}!`);
 });
+
